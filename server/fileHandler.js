2d1c9b4bc0b26f7d0f1ba0264c1d7387d54f10e7
526135164@qq.com <1stBl00d>
2016-03-21 22:47:07 +0800

    judge the files reading from the location

--- server/fileHandler.js
+++ server/fileHandler.js
@@ -21,19 +21,21 @@ var fileHandler = function(req, res){
 var readDir = function(url, res){
     fs.readdir(url, function(err, files){
         var result = [];
-        files.forEach(function(key, value){
-            if(key.charAt(0) != "."){
-                var obj = {
-                    "path" : url + key,
-                    "name" : key
-                };
-                obj.extName = path.extname(obj.path).toLowerCase();
-                obj.stat = fs.statSync(obj.path);
-                obj.isFile = obj.stat.isFile();
-                obj.isDir = obj.stat.isDirectory();
-                result.push(obj);
-            }
-        });
+        if(files){
+            files.forEach(function(key, value){
+                if(key.charAt(0) != "."){
+                    var obj = {
+                        "path" : url + key,
+                        "name" : key
+                    };
+                    obj.extName = path.extname(obj.path).toLowerCase();
+                    obj.stat = fs.statSync(obj.path);
+                    obj.isFile = obj.stat.isFile();
+                    obj.isDir = obj.stat.isDirectory();
+                    result.push(obj);
+                }
+            });
+        }
         res.write(JSON.stringify(result));
         res.end();
     });
