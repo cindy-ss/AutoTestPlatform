538e8ac36260dd3c391f4ef639ed90de65e0d237
edel.ma <526135164@qq.com>
2017-07-18 11:54:48 +0800

    modify the UA.

--- service/query.js
+++ service/query.js
@@ -14,7 +14,7 @@ basic.init({
 const getHeaders = (url) => {
     return {
         referer: url,
-        "User-Agent": "User-Agent	Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4"
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4"
     };
 };
 
