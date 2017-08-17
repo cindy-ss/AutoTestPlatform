4bbfd4dc3580ca1ab41b2267cf31d93900631ce8
edel.ma <526135164@qq.com>
2017-08-17 15:20:29 +0800

    fix an issue which will cause trans to exit.

--- server/trans.js
+++ server/trans.js
@@ -25,80 +25,93 @@ const fetchTrans = (url, auth, cb) => {
     }
     query.query(url, (err, res) => {
         if (!err && res) {
-            const $ = cheerio.load(res);
+            try {
+                const $ = cheerio.load(res);
 
-            let desc = $("meta[name='Description']").attr('content');
-            let ogDesc = $("meta[property='og:description']").attr('content');
-            let ogTitle = $("meta[property='og:title']").attr('content');
-            let ogImage = $("meta[property='og:image']").attr('content');
-            let title = $("title").text();
+                let desc = $("meta[name='Description']").attr('content');
+                let ogDesc = $("meta[property='og:description']").attr('content');
+                let ogTitle = $("meta[property='og:title']").attr('content');
+                let ogImage = $("meta[property='og:image']").attr('content');
+                let title = $("title").text();
 
-            let ogURLObj = URL.parse(ogImage);
-            ogURLObj.host = URL.parse(url).host;
-            ogImage = URL.format(ogURLObj);
+                let ogURLObj = URL.parse(ogImage);
+                ogURLObj.host = URL.parse(url).host;
+                ogImage = URL.format(ogURLObj);
 
-            let obj = {
-                url,
-                desc: desc,
-                ogDesc,
-                ogTitle,
-                ogImage: {
-                    url: ogImage
-                },
-                title,
-            };
+                let obj = {
+                    url,
+                    desc: desc,
+                    ogDesc,
+                    ogTitle,
+                    ogImage: {
+                        url: ogImage
+                    },
+                    title,
+                };
 
-            async.parallel([
-                callback => {
-                    file.getImageSizeByUrl(ogImage, (err, ogSize) => {
-                        callback(err, ogSize)
-                    }, auth);
-                },
-                callback => {
-                    adapter.wechatHandler(res, (err, res) => {
-                        if (!err) {
-                            if (res) {
-                                const wechat_url = URL.resolve(url, res);
+                async.parallel([
+                    callback => {
+                        file.getImageSizeByUrl(ogImage, (err, ogSize) => {
+                            callback(err, ogSize)
+                        }, auth);
+                    },
+                    callback => {
+                        adapter.wechatHandler(res, (err, res) => {
+                            if (!err) {
+                                if (res) {
+                                    const wechat_url = URL.resolve(url, res);
 
-                                file.getImageSizeByUrl(wechat_url, (err, wechat_size) => {
-                                    if (!err) {
-                                        callback(err, {
-                                            url: wechat_url,
-                                            size: wechat_size
-                                        });
-                                    } else {
-                                        callback(null, {
-                                            url: wechat_url,
-                                            size: {}
-                                        })
-                                    }
-                                }, auth);
+                                    file.getImageSizeByUrl(wechat_url, (err, wechat_size) => {
+                                        if (!err) {
+                                            callback(err, {
+                                                url: wechat_url,
+                                                size: wechat_size
+                                            });
+                                        } else {
+                                            callback(null, {
+                                                url: wechat_url,
+                                                size: {}
+                                            })
+                                        }
+                                    }, auth);
+                                } else {
+                                    callback(err, {
+                                        url: null,
+                                        size: {}
+                                    })
+                                }
                             } else {
-                                callback(err, {
-                                    url: null,
-                                    size: {}
-                                })
+                                callback(err, null);
                             }
-                        } else {
-                            callback(err, null);
-                        }
-                    }, auth)
-                }
-            ], function (err, results) {
-                if (!err) {
-                    if (results) {
-                        obj.wechat = {
-                            url: results[1].url,
-                            size: results[1].size
-                        };
-                        obj.ogImage.size = results[0];
+                        }, auth)
                     }
-                    cb(err, obj);
-                } else {
-                    console.log(`\t[ X ] : Fetching shared images on ${url} failed, with an error of ${err.message}`);
-                    cb(err, obj);
-                }
-            });
+                ], function (err, results) {
+                    if (!err) {
+                        if (results) {
+                            obj.wechat = {
+                                url: results[1].url,
+                                size: results[1].size
+                            };
+                            obj.ogImage.size = results[0];
+                        }
+                        cb(err, obj);
+                    } else {
+                        console.log(`\t[ X ] : Fetching shared images on ${url} failed, with an error of ${err.message}`);
+                        cb(err, obj);
+                    }
+                });
+            }
+            catch (e) {
+                console.log(`\t[ X ] : Querying data from ${url} failed, with an error of ${e.message}`);
+                let obj = {
+                    url,
+                    desc: "Bad Link",
+                    ogDesc: "NA",
+                    title: 'NA',
+                    ogTitle: 'NA'
+                };
+                cb(null, obj);
+            }
         } else {
             console.log(`\t[ X ] : Querying data from ${url} failed, with an error of ${err.message}`);
             let obj = {
@@ -117,7 +130,7 @@ const runTask = (urlStr, auth, cb) => {
     let urlArr = urlStr.split('\n');
     async.map(urlArr, (item, callback) => {
         fetchTrans(item, auth, (err, data) => {
-            if(err){
+            if (err) {
                 console.log(`\t[ X ] : Doing meta check failed on ${item}`);
             }
             callback(null, data);
@@ -173,10 +186,10 @@ const dealHTML = (content, cb) => {
             <tr>
                 <td><div><a href="${item.url}" target="_blank">${item.url}</a></div></td>
                 <td><div>${item.title}</div></td>
-                <td${item.desc&&(item.desc.length > 150 || item.desc.length < 100) ? " class='red'" : ""}><div>${item.desc}</div></td>
+                <td${item.desc && (item.desc.length > 150 || item.desc.length < 100) ? " class='red'" : ""}><div>${item.desc}</div></td>
                
                 <td><div>${item.ogTitle}</div></td>
-                <td${item.ogDesc&&(item.ogDesc.length > 150 || item.ogDesc.length < 100) ? " class='red'" : ""}><div>${item.ogDesc}</div></td>
+                <td${item.ogDesc && (item.ogDesc.length > 150 || item.ogDesc.length < 100) ? " class='red'" : ""}><div>${item.ogDesc}</div></td>
                 <td class="text-center"><div><img src="${item.ogImage.url}" alt="ogImage" class="ext-thumb"><br>
                 Width:${item.ogImage.size.width}.Hight:${item.ogImage.size.height}</div></td>
                 <td><div><a href="${item.ogImage.url}" target="_blank">${item.ogImage.url} </div></td>
