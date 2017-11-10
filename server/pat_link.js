5a85365bc0d6f60c58a0292ba36ac55154e2f181
Edel Ma <qun_ma@apple.com>
2017-11-10 13:02:38 +0800

    modify pat link interface to receive multi-line urls.

--- server/pat_link.js
+++ server/pat_link.js
@@ -8,11 +8,26 @@ const getLinks = (url, auth) => {
     let arr = [];
 
     urlArr.map(item => {
-        arr.push(util.urlNormalize(item));
+        arr.push(run(util.urlNormalize(item), auth));
     });
-    // url = util.urlNormalize(url);
 
-    const patLinks = new PatLinks(arr, testsToRun, auth);
+    // let a = new run;
+    // a._runPatLinksTests(arr).then(finalResult => {
+    //     if (finalResult.success.length === 0) {
+    //         return this._displayHelpAndExit("\n\nNo Valid urls provided\n\n");
+    //     }
+    //     const oneUrl = Object.keys(finalResult.success[0])[1];
+    //     const testClasses = _.uniq(finalResult.success[0][oneUrl].map(t => t._type));
+    //     const reportData = this.reporter._createHTMLData(testClasses, finalResult);
+    //
+    //     console.log(reportData);
+    // });
+
+    return Promise.all(arr);
+};
+
+let run = (url, auth) => {
+    const patLinks = new PatLinks(url, testsToRun, auth);
 
     return patLinks.init().then(patLinks.run.bind(patLinks));
 };
