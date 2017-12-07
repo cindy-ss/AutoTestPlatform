81f319b0d942e1c3bd057a685de393f11fe21f7c
weiliang.liu <1984383796@qq.com>
2017-12-07 18:21:25 +0800

    copy copy

--- service/copy.js
+++ service/copy.js
@@ -8,74 +8,178 @@ let compare = (url, auth, cb) => {
     ogURLObj['host'] = 'www.apple.com';
     let online_url = URL.format(ogURLObj);
 
-    Array.prototype.remove = function (val) {
-        let index = this.indexOf(val);
-        if (index > -1) {
-            this.splice(index, 1);
-        }
-    };
+    // Array.prototype.remove = function (val) {
+    //     let index = this.indexOf(val);
+    //     if (index > -1) {
+    //         this.splice(index, 1);
+    //     }
+    // };
 
-    //todo 可以使用URL这个包解析出路径数组，然后判断是否有CN，这样比较安全。
-    //todo usurl和newurl这样的命名不符合规范，请使用下划线或者驼峰。IDE会标注出typo
-    let new_url=URL.parse(url).pathname;
-    let us_url;
-    if (new_url.split('/').indexOf('cn')!==-1) {
-        new_url.remove('cn');
-        us_url = new_url.join('/');
-        //todo 这两个赋值在两个判断分支里是重复的。
-        online_url = url;
-
-    } else {
-
-        url = url;
-        online_url = url;
-        usurl = url;
-
-    }
+    // let new_url=URL.parse(url).pathname;
+    // if (new_url.indexOf('cn')===-1) {
+    //     online_url = url;
+       let us_url = url;
+    //
+    // } else {
+    //
+    //     return false;
+    // }
 
 
-    console.log(`${online_url} VS ${url} VS ${usurl}\n`);
+    // console.log(`${online_url} VS ${url} VS ${us_url}\n`);
 
     async.parallel([
         callback => {
             query.query(online_url, callback, auth);
         },
         callback => {
-            query.query(usurl, callback, auth);
+            query.query(us_url, callback, auth);
         },
         callback => {
-            query.query(url, callback, auth);
+        query.query(url,callback,auth);
         }
+
     ], function (err, res) {
         let arr = [[], [], []];
 
-
         if (!err && res[0] && res[1] && res[2]) {
-            arr = _comp(_getContent(res[0]), _getContent(res[1]), _getContent(res[2]));
-            // console.log("aaaaa");
+            arr = _comp(_getContent(res[0]), _getContent(res[1]),_getContent(res[2]));
             // console.log(_getContent(res[0]));
-            // console.log(_getContent(res[1]));
             // console.log(_getContent(res[2]));
         } else {
             console.log(`\t[ X ] : Fetching shared images on ${url} failed, with an error of ${err.message}`);
             // console.log(`${online_url} missed:\n\t${arr[0].join('\n\t')}\n\n${url} added:\n\t${arr[1].join('\n\t')}`);
         }
-        //todo 注意命名规范。
-        let durl = url.split('/');
-        let aurl = durl[2].split('.');
-        let flag;
-        //todo Flag声明后，赋值，之后未被使用，IDE有提示。
-        if (aurl[0] === "www") {
-            flag = 1;
-            arr.unshift(url);
-        }
-        if (aurl[0] !== "www") {
-            flag = 0;
-            arr.unshift(url);
-        }
-        arr[1] = _getContent(res[0]);
+
+        arr.unshift(url);
+        // console.log(arr);
+        // arr[2] = _getContent(res[0]);
+        // console.log(arr);
         cb(err, arr);
     });
+
+    let _getContent = (str) => {
+        let $ = cheerio.load(str);
+        let top_url = url.split('/');
+        if (top_url.length >5) {
+            var flag = 1;
+        }else{
+            var flag = 0;
+
+        }
+        if(flag===1) {
+
+            //let _text = $('main,#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs,.section-buystrip-hero').text();yijingzhushide
+            let _text;
+            if ($(".section-buystrip-hero")) {
+                if ($(".section-buystrip-hero").parent().is(".main")) {
+                    if ($("main")) {
+                        if ($("main").parent().is("main")) {
+                            _text = $('#main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs').text();
+                        } else {
+                            _text = $('main,#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs').text();
+                        }
+                    }
+                    else {
+                        _text = $('#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs');
+                    }
+                } else {
+                    _text = $('main,#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs,.section-buystrip-hero').text();
+                }
+            } else {
+                _text = $('main,#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs').text();
+            }
+            let textArr = _text.split('\n'), arr = [];
+            textArr.map(item => {
+                let temp = item.replace(/\t/g, '');
+                if (temp.length > 0 && temp.replace(/ /g, '').length > 0) {
+                    arr.push(temp);
+                }
+            });
+
+            return arr;
+        }else {
+
+
+            // 表盘
+            console.log("aaa");
+            let _text2 = $(".gallery-watch");
+            let arr_pan = [];//表盘属性
+            let pan_obj = {animation: [], animation_locale: [], aria_label: [], id: []};
+            _text2.each(function (i, elem) {
+                pan_obj.animation[i] = $(this).attr("data-animation");
+                pan_obj.animation_locale[i] = $(this).attr('data-animation-locale');
+                pan_obj.aria_label[i] = $(this).attr('aria-label');
+                pan_obj.id[i] = $(this).attr('id');
+                arr_pan.push("data-animation:" + pan_obj.animation[i] + " " + "data-animation-locale:" + pan_obj.animation_locale[i] + " " + "aria-label:" + pan_obj.aria_label[i] + " " + 'id:' + pan_obj.id[i]);
+            });
+            arr_pan.shift();
+            //表款
+            let kua_obj = {
+                casing_name: [],
+                band_name: [],
+                aria_label: [],
+                data_size: [],
+                sku_model: [],
+                string_size: [],
+                id: []
+            };
+            let arr_kua = [];//表款属性
+            let _text3 = $("div.gallery-band-visible,div#gallery-cases,div.gallery-content-pane");
+            _text3.each(function (i, elem) {
+                kua_obj.aria_label[i] = $(this).attr('aria-label');
+                kua_obj.casing_name[i] = $(this).attr('data-casing-name');
+                kua_obj.band_name[i] = $(this).attr('data-band-name');
+                kua_obj.sku_model[i] = $(this).attr('data-sku-model');
+                kua_obj.data_size[i] = $(this).attr('data-size');
+                kua_obj.string_size[i] = $(this).attr('data-string-size');
+                kua_obj.id[i] = $(this).attr('id');
+                if (kua_obj.aria_label.join(',').indexOf("表带") === -1) {
+                    arr_kua.push('aria-label:' + kua_obj.aria_label[i] + " " + "data-casing-name:" + kua_obj.casing_name[i] + " " + "data-band-name:" + kua_obj.band_name[i] + " " + 'data-size:' + kua_obj.data_size[i] + "mm" + " " + 'data-sku-model:' + kua_obj.sku_model[i] + " " + 'id:' + kua_obj.id[i]);
+                } else {
+                    arr_kua.push('aria-label:' + kua_obj.aria_label[i] + " " + "data-casing-name:" + kua_obj.casing_name[i] + " " + "data-band-name:" + kua_obj.band_name[i] + " " + 'data-size:' + kua_obj.data_size[i] + "毫米" + " " + 'data-sku-model:' + kua_obj.sku_model[i] + " " + 'id:' + kua_obj.id[i]);
+                }
+            });
+            arr_kua.shift();
+            arr_kua.shift();
+            //表带
+            let _text4 = $(".gallery-band-container,#gallery-bands");
+            let dai_obj = {
+                data_size: [],
+                data_background: [],
+                band_available: [],
+                band_unavailable: [],
+                data_hidden: [],
+                aria_label: [],
+                data_buy: [],
+                id: []
+            };
+            let arr_dai = [];//表带属性
+            _text4.each(function (i, elem) {
+                dai_obj.data_size[i] = $(this).attr("data-size");
+                dai_obj.data_background[i] = $(this).attr("data-background");
+                dai_obj.band_available[i] = $(this).attr("data-band-available");
+                dai_obj.band_unavailable[i] = $(this).attr("data-band-unavailable-copy");
+                dai_obj.data_hidden[i] = $(this).attr("data-hidden");
+                dai_obj.aria_label[i] = $(this).attr("aria-label");
+                dai_obj.data_buy[i] = $(this).attr("data-buy");
+                dai_obj.id[i] = $(this).attr('id');
+                if (dai_obj.aria_label.join(',').indexOf("表带") === -1) {
+                    arr_dai.push('aria-label:' + dai_obj.aria_label[i] + " " + 'data-size:' + dai_obj.data_size[i] + "mm" + " " + 'data-background:' + dai_obj.data_background[i] + " " + 'data-band-available:' + dai_obj.band_available[i] + " " + "data-band-unavailable-copy:" + dai_obj.band_unavailable[i] + " " + "data-hidden:" + dai_obj.data_hidden[i] + " " + "data-buy:" + dai_obj.data_buy[i] + " " + 'id:' + dai_obj.id[i]);
+                } else {
+                    arr_dai.push('aria-label:' + dai_obj.aria_label[i] + " " + 'data-size:' + dai_obj.data_size[i] + "毫米" + " " + 'data-background:' + dai_obj.data_background[i] + " " + 'data-band-available:' + dai_obj.band_available[i] + " " + "data-band-unavailable-copy:" + dai_obj.band_unavailable[i] + " " + "data-hidden:" + dai_obj.data_hidden[i] + " " + "data-buy:" + dai_obj.data_buy[i] + " " + 'id:' + dai_obj.id[i]);
+                }
+            });
+            arr_dai.shift();
+            let arr = new Array();
+            arr[0] = arr_kua;
+            arr[1] = arr_dai;
+            arr[2] = arr_pan;
+            return arr;
+        }
+
+
+    };
 };
 
 
@@ -89,135 +193,16 @@ let _comp = (baseArr, targetArr) => {
             sameArr.push(targetArr[i]);
         }
     }
-    // console.log('abc');
-    // console.log(targetArr);
+
     for (let j = 0; j < baseArr.length; j++) {
         if (targetArr.indexOf(baseArr[j]) === -1) {
             oldArr.push(baseArr[j]);
         }
     }
-    // console.log(baseArr);
+
     return [sameArr, oldArr, newArr];
 };
 
 
-let _getContent = (str) => {
-    let $ = cheerio.load(str);
-
-    //todo 注意let的使用范围。
-    if (flag = 0) {
-
-
-        //let _text = $('main,#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs,.section-buystrip-hero').text();yijingzhushide
-        let _text;
-        if ($(".section-buystrip-hero")) {
-            if ($(".section-buystrip-hero").parent().is(".main")) {
-                if ($("main")) {
-                    if ($("main").parent().is("main")) {
-                        _text = $('#main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs').text();
-                    } else {
-                        _text = $('main,#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs').text();
-                    }
-                }
-                else {
-                    _text = $('#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs');
-                }
-            } else {
-                _text = $('main,#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs,.section-buystrip-hero').text();
-            }
-        } else {
-            _text = $('main,#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs').text();
-        }
-        let textArr = _text.split('\n'), arr = new Array();
-        textArr.map(item => {
-            let temp = item.replace(/\t/g, '');
-            if (temp.length > 0 && temp.replace(/ /g, '').length > 0) {
-                arr.push(temp);
-            }
-        });
-
-    }
-    else {
-
-
-        // biao pan
-        let _text2 = $(".gallery-watch");
-        //todo 建议用 obj = {animation : [], animation_local : []} 这种形式，不然arr2 ~ arr20,无法理解，加了注释也没啥作用。
-        let arr2 = [];// data-animation
-        let arr3 = [];// data-animation-locale
-        let arr4 = [];// aria-label
-        let arrPid = [];//id
-        let arrPan = [];//biao pan shu xing
-        _text2.each(function (i, elem) {
-            arr2[i] = $(this).attr("data-animation");
-            arr3[i] = $(this).attr('data-animation-locale');
-            arr4[i] = $(this).attr('aria-label');
-            arrPid[i] = $(this).attr('id');
-            arrPan.push("data-animation:" + arr2[i] + " " + "data-animation-locale:" + arr3[i] + " " + "aria-label:" + arr4[i] + " " + 'id:' + arrPid[i]);
-        });
-        arrPan.shift();
-        //biao kuan
-        let arr5 = [];//data-casing-name
-        let arr6 = [];//data-band-name
-        let arr7 = [];//aria-label
-        let arr16 = [];//data-size
-        let arr8 = [];//data-sku-model
-        let arr20 = [];//data-string-size
-        let arrKid = [];//id
-        let arrKuan = [];//biao kuan shuxing
-        let _text3 = $("div.gallery-band-visible,div#gallery-cases,div.gallery-content-pane");
-        _text3.each(function (i, elem) {
-            arr7[i] = $(this).attr('aria-label');
-            arr5[i] = $(this).attr('data-casing-name');
-            arr6[i] = $(this).attr('data-band-name');
-            arr8[i] = $(this).attr('data-sku-model');
-            arr16[i] = $(this).attr('data-size');
-            arr20[i] = $(this).attr('data-string-size');
-            arrKid[i] = $(this).attr('id');
-            if (arr7.join(',').indexOf("表带") == -1) {
-                arrKuan.push('aria-label:' + arr7[i] + " " + "data-casing-name:" + arr5[i] + " " + "data-band-name:" + arr6[i] + " " + 'data-size:' + arr16[i] + "mm" + " " + 'data-sku-model:' + arr8[i] + " " + 'id:' + arrKid[i]);
-            } else {
-                arrKuan.push('aria-label:' + arr7[i] + " " + "data-casing-name:" + arr5[i] + " " + "data-band-name:" + arr6[i] + " " + 'data-size:' + arr16[i] + "毫米" + " " + 'data-sku-model:' + arr8[i] + " " + 'id:' + arrKid[i]);
-
-            }
-        });
-        arrKuan.shift();
-        arrKuan.shift();
-        //biao dai
-        let _text4 = $(".gallery-band-container,#gallery-bands");
-        let arr9 = [];//data-size
-        let arr10 = [];//data-background
-        let arr11 = [];//data-band-available
-        let arr12 = [];//data-band-unavailable-copy
-        let arr13 = [];//data-hidden
-        let arr14 = [];//aria-label
-        let arr15 = [];//data-buy
-        let arrDid = [];//id
-        let arrDai = [];//biao dai shuxing
-        _text4.each(function (i, elem) {
-            arr9[i] = $(this).attr("data-size");
-            arr10[i] = $(this).attr("data-background");
-            arr11[i] = $(this).attr("data-band-available");
-            arr12[i] = $(this).attr("data-band-unavailable-copy");
-            arr13[i] = $(this).attr("data-hidden");
-            arr14[i] = $(this).attr("aria-label");
-            arr15[i] = $(this).attr("data-buy");
-            arrDid[i] = $(this).attr('id');
-            if (arr14.join(',').indexOf("表带") == -1) {
-                arrDai.push('aria-label:' + arr14[i] + " " + 'data-size:' + arr9[i] + "mm" + " " + 'data-background:' + arr10[i] + " " + 'data-band-available:' + arr11[i] + " " + "data-band-unavailable-copy:" + arr12[i] + " " + "data-hidden:" + arr13[i] + " " + "data-buy:" + arr15[i] + " " + 'id:' + arrDid[i]);
-            } else {
-                arrDai.push('aria-label:' + arr14[i] + " " + 'data-size:' + arr9[i] + "毫米" + " " + 'data-background:' + arr10[i] + " " + 'data-band-available:' + arr11[i] + " " + "data-band-unavailable-copy:" + arr12[i] + " " + "data-hidden:" + arr13[i] + " " + "data-buy:" + arr15[i] + " " + 'id:' + arrDid[i]);
-            }
-        });
-        arrDai.shift();
-        let arr = new Array();
-        arr[0] = arrKuan;
-        arr[1] = arrDai;
-        arr[2] = arrPan;
-        return arr;
-    }
-
-
-};
 
 exports.compare = compare;
