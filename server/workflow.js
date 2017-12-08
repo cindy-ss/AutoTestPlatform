3e7c053c6355a7a1730bf5327292d09099c1ae2e
Edel Ma <qun_ma@apple.com>
2017-12-08 18:02:19 +0800

    Basic meta function to integrated into workflow.

--- server/workflow.js
+++ server/workflow.js
@@ -1,6 +1,29 @@
+const meta = require('./trans');
+
 //do the flow run.
 //collect the basic flow.
+const run = async options => {
+    // let res = await Promise.all()
+    let auth = options.auth,
+        url = options.url;
 
+    let res = {
+        meta : options.meta ? await metaCheck(url, auth) : null,
+    };
 
-const run = async checkObj => {
+    return res;
 };
+
+let metaCheck = (url, auth) => {
+    return new Promise((resolve, reject) => {
+        meta.runTask(url, auth, (data) => {
+            if(data){
+                resolve(data);
+            }else{
+                reject(data);
+            }
+        });
+    });
+};
+
+exports.run = run;
