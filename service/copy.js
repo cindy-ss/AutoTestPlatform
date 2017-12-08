9bb48db66ec60a23128024c8ab9b191fbfc5e7a1
weiliang.liu <1984383796@qq.com>
2017-12-08 11:36:08 +0800

    copy

--- service/copy.js
+++ service/copy.js
@@ -3,72 +3,72 @@ const query = require('../service/query'),
     async = require('async'),
     cheerio = require('cheerio');
 
-let compare = (url, auth, cb) => {
-    let ogURLObj = URL.parse(url);
-    ogURLObj['host'] = 'www.apple.com';
-    let online_url = URL.format(ogURLObj);
+let autoplay = () =>{
+    let flag;
+    let compare = (url, auth, cb) => {
+        let ogURLObj = URL.parse(url);
+        ogURLObj['host'] = 'www.apple.com';
+        let online_url = URL.format(ogURLObj);
+        let new_url=URL.parse(url).pathname;
+        let us_url;
+        if (new_url.indexOf('cn')!==-1) {
+            online_url = url;
+            us_url = url.replace('/cn','');
 
-    // Array.prototype.remove = function (val) {
-    //     let index = this.indexOf(val);
-    //     if (index > -1) {
-    //         this.splice(index, 1);
-    //     }
-    // };
-
-    // let new_url=URL.parse(url).pathname;
-    // if (new_url.indexOf('cn')===-1) {
-    //     online_url = url;
-       let us_url = url;
-    //
-    // } else {
-    //
-    //     return false;
-    // }
-
-
-    // console.log(`${online_url} VS ${url} VS ${us_url}\n`);
-
-    async.parallel([
-        callback => {
-            query.query(online_url, callback, auth);
-        },
-        callback => {
-            query.query(us_url, callback, auth);
-        },
-        callback => {
-        query.query(url,callback,auth);
-        }
-
-    ], function (err, res) {
-        let arr = [[], [], []];
-
-        if (!err && res[0] && res[1] && res[2]) {
-            arr = _comp(_getContent(res[0]), _getContent(res[1]),_getContent(res[2]));
-            // console.log(_getContent(res[0]));
-            // console.log(_getContent(res[2]));
         } else {
-            console.log(`\t[ X ] : Fetching shared images on ${url} failed, with an error of ${err.message}`);
-            // console.log(`${online_url} missed:\n\t${arr[0].join('\n\t')}\n\n${url} added:\n\t${arr[1].join('\n\t')}`);
+            us_url = url;
+          let  new_arr = new_url.split('/');
+               new_arr.splice(0,1,'/cn');
+               new_url = new_arr.join('/');
+            online_url = URL.parse(url).protocol + "//" + URL.parse(url).host + new_url;
+               url = online_url;
         }
 
-        arr.unshift(url);
-        // console.log(arr);
-        arr[2] = _getContent(res[0]);
-        // console.log(arr);
-        cb(err, arr);
-    });
 
+        console.log(`${online_url} VS ${url} VS ${us_url}\n`);
+
+        async.parallel([
+            callback => {
+                query.query(online_url, callback, auth);
+            },
+            callback => {
+                query.query(us_url, callback, auth);
+            },
+            callback => {
+                query.query(url, callback, auth);
+            }
+
+        ], function (err, res) {
+            let arr = [[], [], []];
+
+            if (!err && res[0] && res[1] && res[2]) {
+                arr = _comp(_getContent(res[0]), _getContent(res[1]), _getContent(res[2]));
+                // console.log(_getContent(res[0]));
+                // console.log(_getContent(res[2]));
+            } else {
+                console.log(`\t[ X ] : Fetching shared images on ${url} failed, with an error of ${err.message}`);
+                // console.log(`${online_url} missed:\n\t${arr[0].join('\n\t')}\n\n${url} added:\n\t${arr[1].join('\n\t')}`);
+            }
+
+
+            arr.unshift(url);
+            // arr[1] = arr[2];
+            cb(err, arr);
+        });
+
+
+    if (URL.parse(url).pathname.indexOf('interactive-gallery') === -1 ){
+        flag = 0;
+    } else {
+        flag = 1;
+
+    }
+    };
     let _getContent = (str) => {
         let $ = cheerio.load(str);
-        let top_url = url.split('/');
-        if (top_url.length >5) {
-            var flag = 1;
-        }else{
-            var flag = 0;
 
-        }
-        if(flag===0) {
-
+        if (flag === 0) {
+            // console.log('addd');
             //let _text = $('main,#main,.main,section.ac-gf-sosumi,nav.ac-gf-breadcrumbs,.section-buystrip-hero').text();yijingzhushide
             let _text;
             if ($(".section-buystrip-hero")) {
@@ -98,18 +98,19 @@ let compare = (url, auth, cb) => {
             });
 
             return arr;
+
         }else {
 
-
+            // console.log('aaaaa');
             // 表盘
             let _text2 = $(".gallery-watch");
             let arr_pan = [];//表盘属性
             let pan_obj = {animation: [], animation_locale: [], aria_label: [], id: []};
             _text2.each(function (i, elem) {
-                pan_obj.animation[i] = $(this).attr("data-animation");
+                pan_obj.animation[i]        = $(this).attr("data-animation");
                 pan_obj.animation_locale[i] = $(this).attr('data-animation-locale');
-                pan_obj.aria_label[i] = $(this).attr('aria-label');
-                pan_obj.id[i] = $(this).attr('id');
+                pan_obj.aria_label[i]       = $(this).attr('aria-label');
+                pan_obj.id[i]               = $(this).attr('id');
                 arr_pan.push("data-animation:" + pan_obj.animation[i] + " " + "data-animation-locale:" + pan_obj.animation_locale[i] + " " + "aria-label:" + pan_obj.aria_label[i] + " " + 'id:' + pan_obj.id[i]);
             });
             arr_pan.shift();
@@ -126,11 +127,11 @@ let compare = (url, auth, cb) => {
             let arr_kua = [];//表款属性
             let _text3 = $("div.gallery-band-visible,div#gallery-cases,div.gallery-content-pane");
             _text3.each(function (i, elem) {
-                kua_obj.aria_label[i] = $(this).attr('aria-label');
+                kua_obj.aria_label[i]  = $(this).attr('aria-label');
                 kua_obj.casing_name[i] = $(this).attr('data-casing-name');
-                kua_obj.band_name[i] = $(this).attr('data-band-name');
-                kua_obj.sku_model[i] = $(this).attr('data-sku-model');
-                kua_obj.data_size[i] = $(this).attr('data-size');
+                kua_obj.band_name[i]   = $(this).attr('data-band-name');
+                kua_obj.sku_model[i]   = $(this).attr('data-sku-model');
+                kua_obj.data_size[i]   = $(this).attr('data-size');
                 kua_obj.string_size[i] = $(this).attr('data-string-size');
                 kua_obj.id[i] = $(this).attr('id');
                 if (kua_obj.aria_label.join(',').indexOf("表带") === -1) {
@@ -155,14 +156,14 @@ let compare = (url, auth, cb) => {
             };
             let arr_dai = [];//表带属性
             _text4.each(function (i, elem) {
-                dai_obj.data_size[i] = $(this).attr("data-size");
-                dai_obj.data_background[i] = $(this).attr("data-background");
-                dai_obj.band_available[i] = $(this).attr("data-band-available");
+                dai_obj.data_size[i]        = $(this).attr("data-size");
+                dai_obj.data_background[i]  = $(this).attr("data-background");
+                dai_obj.band_available[i]   = $(this).attr("data-band-available");
                 dai_obj.band_unavailable[i] = $(this).attr("data-band-unavailable-copy");
-                dai_obj.data_hidden[i] = $(this).attr("data-hidden");
-                dai_obj.aria_label[i] = $(this).attr("aria-label");
-                dai_obj.data_buy[i] = $(this).attr("data-buy");
-                dai_obj.id[i] = $(this).attr('id');
+                dai_obj.data_hidden[i]      = $(this).attr("data-hidden");
+                dai_obj.aria_label[i]       = $(this).attr("aria-label");
+                dai_obj.data_buy[i]         = $(this).attr("data-buy");
+                dai_obj.id[i]               = $(this).attr('id');
                 if (dai_obj.aria_label.join(',').indexOf("表带") === -1) {
                     arr_dai.push('aria-label:' + dai_obj.aria_label[i] + " " + 'data-size:' + dai_obj.data_size[i] + "mm" + " " + 'data-background:' + dai_obj.data_background[i] + " " + 'data-band-available:' + dai_obj.band_available[i] + " " + "data-band-unavailable-copy:" + dai_obj.band_unavailable[i] + " " + "data-hidden:" + dai_obj.data_hidden[i] + " " + "data-buy:" + dai_obj.data_buy[i] + " " + 'id:' + dai_obj.id[i]);
                 } else {
@@ -170,7 +171,7 @@ let compare = (url, auth, cb) => {
                 }
             });
             arr_dai.shift();
-            let arr = new Array();
+            let arr = [];
             arr[0] = arr_kua;
             arr[1] = arr_dai;
             arr[2] = arr_pan;
@@ -179,29 +180,28 @@ let compare = (url, auth, cb) => {
 
 
     };
-};
+    let _comp = (baseArr, targetArr) => {
+        let sameArr = [], oldArr = [], newArr = [];
+        for (let i = 0; i < targetArr.length; i++) {
+            if (baseArr.indexOf(targetArr[i]) === -1) {
+                newArr.push(targetArr[i]);
 
-
-let _comp = (baseArr, targetArr) => {
-    let sameArr = [], oldArr = [], newArr = [];
-    for (let i = 0; i < targetArr.length; i++) {
-        if (baseArr.indexOf(targetArr[i]) === -1) {
-            newArr.push(targetArr[i]);
-
-        } else {
-            sameArr.push(targetArr[i]);
+            } else {
+                sameArr.push(targetArr[i]);
+            }
         }
-    }
 
-    for (let j = 0; j < baseArr.length; j++) {
-        if (targetArr.indexOf(baseArr[j]) === -1) {
-            oldArr.push(baseArr[j]);
+        for (let j = 0; j < baseArr.length; j++) {
+            if (targetArr.indexOf(baseArr[j]) === -1) {
+                oldArr.push(baseArr[j]);
+            }
         }
-    }
 
-    return [sameArr, oldArr, newArr];
+        return [sameArr, oldArr, newArr];
+    };
+
+    exports.compare = compare;
+
+
 };
-
-
-
-exports.compare = compare;
+autoplay();
