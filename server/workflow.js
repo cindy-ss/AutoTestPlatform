c5aecafef0c3e6b1dd154d1e69cc0a819d34d5df
weiliang.liu <1984383796@qq.com>
2017-12-14 15:41:16 +0800

    workflow thursday1

--- server/workflow.js
+++ server/workflow.js
@@ -1,28 +1,48 @@
-const meta = require('./trans'),
-    link = require('./link');
+const util = require('../service/util'),
+    meta = require('./trans'),
+    link = require('./link'),
+    vpath = require('./vpath'),
+    viewport = require('./viewport'),
+    footnote = require('./footnote'),
+    copy = require('../service/copy');
+
 
 //do the flow run.
 //collect the basic flow.
 const run = async options => {
+
     let auth = options.auth,
         url = options.url;
 
     let promises = [
         metaCheck(url, auth),
-        linkCheck(url, auth)
+        linkCheck(url, auth),
+        vpathCheck(url,auth),
+        viewportCheck(url,auth),
+        footnoteCheck(url,auth),
+        copyCheck(url,auth)
+
     ];
 
     let obj = {
         url,
         message: null,
         meta: null,
-        link: null
+        link: null,
+        vpath: null,
+        viewport: null,
+        footnote: null,
+        copy: null
     };
 
     return await Promise.all(promises)
         .then(value => {
             obj['meta'] = value[0];
             obj['link'] = value[1];
+            obj['vpath'] = value[2];
+            obj['viewport'] = value[3];
+            obj['footnote'] = value[4];
+            obj['copy'] = value[5];
 
             return obj;
         }).catch(err => {
@@ -50,6 +70,54 @@ let linkCheck = (url, auth) => {
             if (err) {
                 reject(err);
             } else {
+                resolve(data);
+            }
+        })
+    })
+};
+
+let vpathCheck = (url, auth) => {
+    return new Promise((resolve, reject) => {
+        vpath.getVPaths(url, auth, (err,data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    })
+};
+
+let viewportCheck = (url, auth) => {
+    return new Promise((resolve, reject) => {
+        viewport.runTask(url, auth, (err, data) => {
+            if(err){
+                reject(err);
+            }else{
+                resolve(data);
+            }
+        })
+    })
+};
+
+let footnoteCheck = (url, auth) => {
+    return new Promise((resolve, reject) => {
+        footnote.runTask(url, auth, (err, data) => {
+            if(err) {
+                reject(err);
+            }else {
+                resolve(data);
+            }
+        })
+    })
+};
+
+let copyCheck = (url, auth) => {
+    return new Promise((resolve, reject) => {
+        copy.compare(url, auth, (err, data) => {
+            if(err) {
+                reject(err);
+            }else {
                 resolve(data);
             }
         })
---
+++ test/workflow.test.js
@@ -0,0 +1,22 @@
+let wf = require('../server/workflow'),
+    basic = require('../service/basic');
+
+let options = {
+    url: 'https://www.apple.com/cn/iphone-x',    meta: false,
+    font: false,
+    link: true,
+    vPath: false,
+    viewPort: false,
+    footNote: false,
+    copy: false,
+    auth: {
+        odUser: 'qun_ma',
+        odPass: 'Profero@123'
+    }
+};
+
+basic.init(() => {
+    wf.run(options).then(data => {
+        console.log(data);
+    });
+});
